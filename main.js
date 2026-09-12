15cacdff675b2c0f09491f752b3b4784a05ec487
Harizmatlk <islamriz2011@gmail.com>
2026-09-12 20:11:44 +0300

    pr 2

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const productCard= document.querySelector('.product-card');
-const changeColorCardButton = document.querySelector('#change-color-card-button');
- console.log('hi');
+//Покраска 1 карточки
+const ProductCard=document.querySelector('.product_card');
+const ChangeColorFirstCardButton = document.querySelector('#change-color-first-card-button');
 
-changeColorCardButton.addEventListener('click', () => {
- productCard.style.backgroundColor = 'red';
+ChangeColorFirstCardButton.addEventListener('click', () => {
+    ProductCard.style.backgroundColor = blueColorHash;
 })
+
+//Покраска всех карточек
+const productCards=document.querySelectorAll('.product_card');
+const ChangeColorForCardsButton = document.querySelector('#change-color-for-cards-list-button');
+const greenColorHash = '#00FF00'
+const blueColorHash = '#0000FF'
+ChangeColorForCardsButton.addEventListener('click', () => {
+ productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
+ });
+
+console.log(productCards)
+
+[1,2,3,4,5];
+
+//Открытие гугла
+const OpenGoogleButton=document.querySelector('#Open-Google')
+
+OpenGoogleButton.addEventListener('click', () => {
+    window.open('https://www.google.com/')
+});
